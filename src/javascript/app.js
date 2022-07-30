9ee4095ac29f418eb2eba921f065dba69f8e8fc7
theomccoy <ltheo.mohlomi@gmail.om>
2022-07-30 23:37:25 +0200

    logo back to index.html

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,34 +1,33 @@
-'use strict'
+"use strict";
 
-var mouseCursor = document.querySelector('.cursor');
-var navLinks = document.querySelectorAll('.nav-item');
-var projects = document.querySelectorAll('.project-card');
+var mouseCursor = document.querySelector(".cursor");
+var navLinks = document.querySelectorAll(".nav-item");
+var projects = document.querySelectorAll(".project-card");
 
-var projectH2 = document.getElementsByClassName('benefits-h2');
+var projectH2 = document.getElementsByClassName("benefits-h2");
 
-window.addEventListener('mousemove', e =>{
-    mouseCursor.style.top = e.pageY + 'px';
-    mouseCursor.style.left = e.pageX + 'px';
-    
+window.addEventListener("mousemove", (e) => {
+  mouseCursor.style.top = e.pageY + "px";
+  mouseCursor.style.left = e.pageX + "px";
 });
 
-navLinks.forEach(link =>{
-    link.addEventListener('mouseover', () =>{
-        mouseCursor.classList.add("link-grow");
-        link.classList.add('hovered-link')
-    });
+navLinks.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("link-grow");
+    link.classList.add("hovered-link");
+  });
 
-    link.addEventListener('mouseleave', () =>{
-        mouseCursor.classList.remove('link-grow');
-        link.classList.remove('hovered-link')
-    })
+  link.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("link-grow");
+    link.classList.remove("hovered-link");
+  });
 });
 
-projects.forEach(project =>{
-    project.addEventListener('mousevoer', ()=>{
-        projectH2.classList.add('project-hovered');
-    })
-    project.addEventListener('mouseleave', ()=>{
-        projectH2.classList.remove('project-hovered');
-    })
-})
+projects.forEach((project) => {
+  project.addEventListener("mousevoer", () => {
+    projectH2.classList.add("project-hovered");
+  });
+  project.addEventListener("mouseleave", () => {
+    projectH2.classList.remove("project-hovered");
+  });
+});
